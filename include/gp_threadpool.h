8fb4ea209aa16337d60354385920af307f856a66
Sicily9 <727847140@qq.com>
2019-05-19 19:24:34 +0800

    task processor finish with unit test

--- include/gp_threadpool.h
+++ include/gp_threadpool.h
@@ -7,33 +7,44 @@
 typedef void (*gp_cb)(void *);
 struct gp_taskq;
 
-struct gp_task {
+typedef struct gp_task {
 	struct gp_list_node      task_node;
 	void *	     		 task_arg;
 	gp_cb	     		 task_cb;
-	struct gp_taskq		 task_tq;
+	struct gp_task_processor *task_tp;
 	struct gp_thread_manager *task_tmr;
 	unsigned		 task_busy;
-	int			 task_prep;
-	int			 task_reap;
+	//int			 task_prep;
+	//int			 task_reap;
 	struct gp_mtx		 task_mtx;
 	struct gp_cv	         task_cv;
-};
+}gp_task;
 
-struct gp_taskq_tmr {
-	struct gp_taskq * tqt_tq;
-	struct gp_thread_manager tqt_thread;
-};
+typedef struct gp_taskp_tmr {
+	struct gp_task_processor * tpt_tp;
+	struct gp_thread_manager tpt_thread;
+}gp_taskp_tmr;
 
-struct gp_taskq {
-	struct gp_list	     tq_tasks;
-	struct gp_mtx	     tq_mtx;
-	struct gp_cv	     tq_sched_cv;
-	struct gp_cv	     tq_wait_cv;
-	struct gp_taskq_tmr *tq_threads;
-	int		     tq_nthreads;
-	int		     tq_run;
-};
 
+// a threadpool
+typedef struct gp_task_processor {
+	struct gp_list	     tp_tasks;
+	struct gp_mtx	     tp_mtx;
+	struct gp_cv	     tp_sched_cv;
+	struct gp_cv	     tp_wait_cv;
+	struct gp_taskp_tmr *tp_threads;
+	int		     tp_nthreads;
+	int		     tp_run;
+} gp_task_processor;
+
+extern void create_task_processor(gp_task_processor **gp_tp, int num);
+extern void init_task_processor(gp_task_processor *gp_tp, int num);
+extern void destroy_task_processor(gp_task_processor *gp_tp);
+
+extern void create_task(gp_task ** task, gp_task_processor *gp_tp, gp_cb cb, void *arg);
+extern void init_task(gp_task *task, gp_task_processor *gp_tp, gp_cb cb, void *arg);
+extern void run_task(gp_task *task);
+extern void wait_task(gp_task *task);
+extern void destroy_task(gp_task *task);
 
 #endif
