044906863e22be54c0b95901e1dd456fdc262694
Sicily9 <727847140@qq.com>
2019-05-19 19:25:30 +0800

    task processor finish with unit test

---
+++ test/threadpool/test_threadpool.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "gp_threadpool.h"
+#include "task.pb-c.h"
+
+
+void func1(void *arg)
+{
+	Name *a = arg;
+	printf("%s\n", a->name);
+}
+
+int main()
+{
+	Name a;
+	name__init(&a);
+	a.name = malloc(10);
+	strcpy(a.name, "hello");
+	
+	gp_task_processor *gp_tp;
+	create_task_processor(&gp_tp,2);
+	destroy_task_processor(gp_tp);
+
+	gp_task_processor *gp_tp2;
+	create_task_processor(&gp_tp2,2);
+
+	gp_task *task;
+	create_task(&task, gp_tp2, func1, &a);
+	run_task(task);
+
+	destroy_task(task);
+	destroy_task_processor(gp_tp2);
+
+	free(a.name);
+	return 0;
+}
