c216c592d3a65a083d7179517acd87004688ce52
Sicily9 <727847140@qq.com>
2019-06-12 19:52:39 +0800

    module

---
+++ test/module/test_module.c
@@ -0,0 +1,24 @@
+#include "gp_module.h"
+
+void early_init_test(void){
+	printf("early_init_test\n");
+}
+
+void init_test(void){
+	printf("init_test\n");
+}
+gp_module_desc test_module = {
+	.name = "test",
+	.type = MODULE_INIT_FIRST,
+	.init = init_test,
+	.early_init = early_init_test,
+};
+
+gp_module_init(test_module);
+
+
+int main()
+{
+	printf("main...\n");
+	return 0;
+}
