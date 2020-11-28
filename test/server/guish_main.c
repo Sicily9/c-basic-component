28cdb97c4f860eb47ff136dc024d6012d854747f
Sicily9 <727847140@qq.com>
2020-11-28 15:12:55 +0800

    fix

---
+++ test/server/guish_main.c
@@ -0,0 +1,21 @@
+#include "guish_server.h"
+
+int main()
+{	
+	printf("EPOLL_CTL_ADD:1 EPOLL_CTL_MOD:3 EPOLL_CTL_DEL:2\n");
+	printf("POLLHUP:16 POLLIN:1 POLLERR:8 POLLNVAL:32 POLLPRI:2 POLLRDHUP:8192 POLLOUT:4\n");
+	gp_loop *loop = NULL;
+	create_gp_loop(&loop);
+
+	gp_inet_address *address = NULL;
+	create_gp_inet_address(&address, "0.0.0.0", 8000, 0);
+
+	guish_server *server = NULL;
+	create_guish_server(&server, loop, address, "guish");
+
+	guish_start_server(server);
+	
+	gp_loop_run(loop, GP_RUN_DEFAULT);
+
+	return 0;
+}
