028c283d21df345b6937ab6f06ba5b05a728d5a9
Sicily9 <727847140@qq.com>
2020-12-09 20:08:52 +0800

    micro fix

--- test/module/test_module.cpp
+++ test/module/test_module.cpp
@@ -4,32 +4,32 @@
 #include <iostream>
 #include "test.h"
 #include "gtest/gtest.h"
+#define _CRT_SECURE_NO_WARNINGS 
 
 long long start = 0;
 long long init = 0;
 long long fuck_end = 0;
 
 using namespace std;
+
 extern "C" {
-#include "gp.h"
-void early_init_test2(void) {
-	printf("test2\n");
-}
 
-void early_init_test(void) {
-    start = timeInMicroseconds();
-}
+    #include "gp.h"
+    void early_init_test2(void) {
+	    printf("test2\n");
+    }
 
-void init_test(void) {
-    init = timeInMicroseconds();
-}
+    void early_init_test(void) {
+        start = timeInMicroseconds();
+    }
 
-//gp_module_desc test_module;
+    void init_test(void) {
+        init = timeInMicroseconds();
+    }
 
-char name[] ="test";
-gp_module_init(test_module, MODULE_INIT_FIRST, name, init_test, early_init_test) ;
+    gp_module_init(test_module, MODULE_INIT_FIRST, "test", init_test, early_init_test) ;
 
-gp_module_init(test2_module, MODULE_INIT_FIRST, "222", init_test, early_init_test2) ;
+    gp_module_init(test2_module, MODULE_INIT_FIRST, "222", init_test, early_init_test2) ;
 
 }
 
